17263c9294bab43a34ef3b6164f9038452850d47
Luívula Filipe <229335794+hemany404@users.noreply.github.com>
2025-12-28 15:17:13 +0100

    auth configurando

--- app/auth/auth.py
+++ app/auth/auth.py
@@ -1,7 +1,7 @@
 from fastapi import Depends, HTTPException
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from fastapi.security import  OAuth2PasswordRequestForm
 from jose import jwt
-from app.main import SECRETY_KEY,ALGORITHM,ACESS_TOKEN_MINUTO_EXPIRACAO
+from app.main import oauth2_schema,SECRETY_KEY,ALGORITHM,ACESS_TOKEN_MINUTO_EXPIRACAO,bcrypt_context
 from sqlalchemy.orm import Session
 from app.models.modelos import Usuario
 from app.core.database import pegar_db
@@ -14,9 +14,11 @@ def criar_token(id_usuario: int, duracao_token = timedelta(minutes=ACESS_TOKEN_M
     return jwt_codificado
 
 def autenticar_usuario(email,senha,session):
-    instituicao = session.query(Usuario).filter(Instituicao.email == email).first()
-    if not instituicao:
+    usuario = session.query(Usuario).filter(Usuario.email == email).first()
+    if not usuario:
         return False
-    elif not bcrypt_context.verify(senha,instituicao.senha):
+    elif not bcrypt_context.verify(senha, usuario.senha):
         return False
-    return instituicao
+    return usuario
+
+async def pegar_usuario(token: str =Depends(oauth2_schema)) 
