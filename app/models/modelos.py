2fd5cce484ec09e4ad1a0c5d81c60fd9444727d3
Luívula Filipe <229335794+hemany404@users.noreply.github.com>
2025-12-28 15:08:52 +0100

    Update modelos.py

--- app/models/modelos.py
+++ app/models/modelos.py
@@ -6,7 +6,7 @@ Base = declarative_base()
 class Usuario(Base):
     __tablename__ = "usuarios"
     id = Column(Integer, primary_key=True, index=True)
-    nome_usuario = Column(String, index=True)
-    email = Column(String,)
-    senha = Column(String)
+    nome_usuario = Column(String,nullable=False, index=True)
+    email = Column(String,unique=True,nullable=False,index=True)
+    senha = Column(String,nullable=False)
     admin = Column(Boolean, default=False)
