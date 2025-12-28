93319b123ba3725bb15831db3b139f4cb23a5a73
Luívula Filipe <229335794+hemany404@users.noreply.github.com>
2025-12-28 16:14:13 +0100

    Update api_financeira.py

--- app/api_financeira.py
+++ app/api_financeira.py
@@ -1,3 +1,8 @@
 import httpx
 
 async def buscar_preco(simbolo:str):
+        url = f"https://api.coingecko.com/api/v3/simple/price?ids={simbolo}&vs_currencies=usd"
+        async with httpx.AsyncClient as cliente:
+                r = await cliente.get(url)
+                data = r.json()
+                return data.get(simbolo, {}).get("usd")
