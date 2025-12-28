373f39dce0dad2d1ae0508a4345d723dc0b71def
Luívula Filipe <229335794+hemany404@users.noreply.github.com>
2025-12-28 17:02:11 +0100

    act

--- alembic/env.py
+++ alembic/env.py
@@ -5,6 +5,11 @@ from sqlalchemy import pool
 
 from alembic import context
 
+import sys
+import os
+
+#para navegar no meu ambiente e encontrar os modelos
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
