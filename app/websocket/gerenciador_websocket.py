40f2abe540a034bc3bbda285beb1c91fbaf8e70e
Luívula Filipe <229335794+hemany404@users.noreply.github.com>
2025-12-28 15:37:28 +0100

    Update gerenciador_websocket.py

--- app/websocket/gerenciador_websocket.py
+++ app/websocket/gerenciador_websocket.py
@@ -1,3 +1,8 @@
 from fastapi import WebSocket
 
-class
+class gerenciador_conexao:
+    def __init__(self):
+        self.usuarios_activos = []
+
+    async def conectar(self,ws:WebSocket):
+        ws.accept()    
