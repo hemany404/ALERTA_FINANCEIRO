457a6241bd0e3c039e6589b903cba7cf94879ce3
Luívula Filipe <229335794+hemany404@users.noreply.github.com>
2025-12-28 17:03:11 +0100

    Update env.py

--- alembic/env.py
+++ alembic/env.py
@@ -22,8 +22,8 @@ if config.config_file_name is not None:
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
+from app.models.modelos import Base
+target_metadata = Base.metada
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
